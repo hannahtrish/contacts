90d155e9a21ddb2ea46dd36232374d925ffd7b84
hannahtrish <134283320+hannahtrish@users.noreply.github.com>
2023-06-12 17:24:25 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.querySelector(contact).addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active")
+});
+
+function show(){
+    alert("You're message has been received. We will get back to you soon.")
+}
